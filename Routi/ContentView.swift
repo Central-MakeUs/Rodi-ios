1cc72c72e192a0a571d6612f7ed12d77e49267ed
윤달 <yundal8755@gmail.com>
2026-06-26 15:50:29 +0900

    Initial Commit

---
+++ Routi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Routi
+//
+//  Created by mac on 6/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Routi/RoutiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutiApp.swift
+//  Routi
+//
+//  Created by mac on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct RoutiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
