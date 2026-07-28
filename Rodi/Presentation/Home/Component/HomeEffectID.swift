bd46f2ac6905179c4b0293b797a9207a4b4f8d48
윤달 <yundal8755@gmail.com>
2026-07-28 18:05:25 +0900

    refactor: 홈 공용 상태와 라우터 구조 정리
    홈 Feature의 공용 상태와 상호작용 코드를 최상위 구조로 정리한다.\n\n- HomeRouter를 홈 Feature 루트로 이동\n- presentation reducer와 effect 식별자 분리\n- presentation state를 공용 Component 경로로 이동

---
+++ Rodi/Presentation/Home/Component/HomeEffectID.swift
@@ -0,0 +1,12 @@
+//
+//  HomeEffectID.swift
+//  Rodi
+//
+
+enum HomeEffectID {
+    static let routeLoading = "home.route.loading"
+    static let placeDetailLoading = "home.place-detail.loading"
+    static let bookmarkUpdating = "home.place-detail.bookmark-updating"
+    static let placeListLoading = "home.place-list.loading"
+    static let snackbarDismissal = "home.presentation.snackbar-dismissal"
+}
